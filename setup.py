87c9d63d160fb7a55721045cf1e4a5cd251f4dc5
John Lin <johnlinp@gmail.com>
2019-04-18 15:30:20 +0800

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='duosql',
-    version='0.1.6',
+    version='0.1.7',
     description='An easy way to demo database transactions.',
     long_description=long_description,
     long_description_content_type='text/markdown',
